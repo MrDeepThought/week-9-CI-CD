7f5395942927aaf6c0f55e6108bc9f846fc24185
MrDeepThought <singharshpreet55@gmail.com>
2023-08-09 14:06:30 +0530

    updated the main index file to connect to database

--- server/index.js
+++ server/index.js
@@ -15,6 +15,6 @@ app.use("/user", userRouter)
 
 // Connect to MongoDB
 // DONT MISUSE THIS THANKYOU!!
-mongoose.connect('mongodb://localhost:27017/courses', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
+mongoose.connect('mongodb://0.0.0.0:27017/CourseDemo', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "CourseDemo" });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
